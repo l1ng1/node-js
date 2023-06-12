2c906d1f650979c283ba2054c5f9fff2b1113d6a
l1ng1 <pk9942601@gmail.com>
2023-06-12 13:49:32 +0600

    почти

---
+++ cowsay dz/maingeometry.js
@@ -0,0 +1,27 @@
+export class Geometry{
+
+    square(width){
+        console.log(`
+        Площадь: ${width**2}
+        Периметр: ${width*4}
+        `);
+    }
+
+    rectangle(height,width){
+        console.log(`
+        Площадь: ${width*height}
+        Периметр: ${(width+height)*2}
+        `);
+    }
+
+    circle(radius){
+        console.log(`
+        Площадь: ${Math.PI*(radius**2)}
+        Периметр: ${Math.PI*radius*2}
+        `);
+    }
+
+}
+
+
+module.exports = Geometry;
---
+++ cowsay dz/showmain.js
@@ -0,0 +1,11 @@
+// const animalsay = require('./main.js');
+
+// let a = new animalsay;
+// a.crabsay("Все получилось!");
+// a.foxsay('Это лис');
+
+
+import {Geometry} from './maingeometry.js';
+
+let b = new Geometry;
+b.square(10);
