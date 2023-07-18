f32e12a5e1992ee7d12b497d59ad8cb4a9bd5b42
l1ng1 <pk9942601@gmail.com>
2023-07-18 12:57:40 +0600

    ok

---
+++ back1/bin/route/route.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+export class Router {
+    constructor(controller, config) {
+        this.controller = controller;
+        this.config = config;
+        this.app = express();
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    start() {
+        this.server = this.app.listen(this.config.port, ()=>{
+            console.log("Server started at", this.config.port);
+        });
+        this.createRoutes()
+    }
+
+    stop() {
+        this.server.close();
+    }
+
+    createRoutes() {
+        this.app.get("/", this.controller.mainUserPage,
+                          this.controller.mainGeneralPage);
+
+        this.app.get("/register", this.controller.mainUserPage,
+                                  this.controller.registrationPage);
+
+        this.app.get("/login", this.controller.mainUserPage,
+                               this.controller.loginPage);
+
+        this.app.post("/confirm", this.controller.mainUserPage,
+                                  this.controller.checkCaptcha,
+                                  this.controller.confirmPage);
+
+        this.app.post("/confirmed", this.controller.mainUserPage,
+                                    this.controller.checkConfirmCode,
+                                    this.controller.redirToUserPage);
+
+    }
+}
--- pogoda/main.js
+++ pogoda/main.js
@@ -1,5 +1,13 @@
-
-
-
-
 const API_KEY = '76718bf6a90d44b9bc8153803232605';
+const BOT_KEY = '6324580848:AAFlLW7sgRcuPU_a4VB0usztf383EbqBS3o';
+
+import TelegramBot from "node-telegram-bot-api";
+
+let chat_id;
+const bot = new TelegramBot(BOT_KEY, {polling: true});
+
+bot.on('text',async msg =>{
+    console.log(msg);
+    chat_id = msg.chat.id;
+    bot.sendMessage(chat_id,'привет');
+})
