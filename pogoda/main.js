cb89b270a025145a9948ee0c0ab877ebf6d8dd6b
l1ng1 <pk9942601@gmail.com>
2023-07-20 11:37:18 +0600

    pogoda

--- pogoda/main.js
+++ pogoda/main.js
@@ -3,22 +3,24 @@ const BOT_KEY = '6324580848:AAFlLW7sgRcuPU_a4VB0usztf383EbqBS3o';
 
 import TelegramBot from "node-telegram-bot-api";
 import fetch from "node-fetch";
+import axios from 'axios';
 let chat_id;
 const bot = new TelegramBot(BOT_KEY, {polling: true});
 
 bot.on('text',async msg =>{
     let chat_id = msg.chat.id;
-    
-    // if(msg === '/start'){
-        bot.sendMessage(chat_id,'Привет ,это бот,который показывает в выброном городе.Назови свой город и узнаю в нем погоду');
-        bot.on('text',async msg=>{
-            let url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${msg.text}&days=10&aqi=no&alerts=no&lang=ru`;
-            let answ;
-            fetch(url)
-                .then(res => res.json())
-                .then(json =>console.log(json));
-        })
-   // }
-    
-   
+    bot.sendMessage(chat_id,'Привет ,это бот,который показывает в выброном городе.Назови свой город и узнаю в нем погоду');
+     
+        let url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${msg.text}&days=10&aqi=no&alerts=no&lang=ru`;
+        let a = await fetch(url);
+        let res = await a.json();
+        if(!res)  return bot.sendMessage(chat_id,'Такого города нет!')
+        let image = await axios.get(res.current.condition.icon, { responseType: 'arraybuffer' });
+        let answ = `
+        Город - ${res.location.name},${res.location.country}
+        Время - ${res.location.localtime.split(' ')[1]}
+        Погода - ${res.current.condition.text}
+        Температура воздуха - ${res.current.temp_c}
+        `
+        bot.sendPhoto(chat_id,image.data,{caption:answ});
 })
